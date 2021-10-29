7c65d17900ea0fae8461b85ec2a070101988fd3c
VARSHITHAGANJI <80575682+VARSHITHAGANJI@users.noreply.github.com>
2021-10-29 10:34:20 +0530

    Update code.py

--- GATE_ASSIGNMENT4/code.py
+++ GATE_ASSIGNMENT4/code.py
@@ -1,19 +1,39 @@
-##Plotting the poles of the system or roots of the characteristic equation
-import numpy as np
 import matplotlib.pyplot as plt
-pole1 = [-0.9,0]
-pole2 = [0.9 , 0]
-pole3 = [0,0]
+import matplotlib.cm     as cm
+from matplotlib.colors import Normalize
+import matplotlib.patches as mpatches
+import matplotlib.lines as mlines
+import numpy as np
 
-theta = np.linspace(0, 2*np.pi, n, endpoint=True)
 
-ax = plt.subplot(111, aspect='equal')
+theta = np.linspace(0, 2*np.pi, 100, endpoint=True)
+# fill color in Circle
+cmap = cm.jet   # Select colormap U want
+# Declare for set range of value for normalization
+vmin = 0        
+vmax = 1
+# Normalize value for cmap
+norm = Normalize(vmin, vmax)
+fig, ax = plt.subplots(figsize = (6,6))
+poles, = plt.plot([0.9,-0.9,0],[0,0,0], 'ro',label = 'Poles')
+patches = mpatches.Patch(color="hotpink", label="ROC : |z|>0.9")
 
-plt.plot(np.sin(theta), np.cos(theta) , 'k-' , label = "Unit Circle: $|z| = 1$")
-plt.plot([pole1[0] , pole2[0],pole3[0]] , [pole1[1],pole2[1],pole3[1]] , 'rx' , label = "Roots of $z^3-0.81z=0$")
+dotted_line = mlines.Line2D([],[],color = 'black',label='Unit circle |z| = 1')
+dotted_line.set_linestyle('-')
+dotted_line1 = mlines.Line2D([],[],color = 'blue',label='Circle |z| = 0.9')
+dotted_line1.set_linestyle('-')
+ax.set_xlim([-2,2])
+ax.set_ylim([-2,2])
+draw_circle = plt.Circle((0, 0), 1,fill = False)
 
-plt.title("Poles plot")
-plt.grid(True)
-plt.legend(loc='upper right')
+p = plt.plot(0.9*np.sin(theta), 0.9*np.cos(theta) , 'k-' , label = "Unit Circle: $|z| = 1$",color='b')
+dr = plt.Circle((0, 0), 0.9,color='white',edgecolor='blue',ls='-')
+plt.gcf().gca().add_artist(draw_circle)
+plt.gcf().gca().add_artist(dr)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Possible ROC of $z^3-0.81z=0$')
+plt.legend(handles=[patches,dotted_line,poles,dotted_line1], loc = 'upper right')
+ax.set_aspect(1)
+ax.set_facecolor('xkcd:pink')
 plt.show()
-
